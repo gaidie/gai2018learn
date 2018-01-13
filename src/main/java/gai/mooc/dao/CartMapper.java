dbe07357e130d06bfeeb314619539f05a98e31c8
zenggencome@163.com <zenggencome@163.com>
2018-01-13 13:59:49 +0800

    项目初始化提交

---
+++ src/main/java/gai/mooc/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package gai.mooc.dao;
+
+import gai.mooc.bean.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
